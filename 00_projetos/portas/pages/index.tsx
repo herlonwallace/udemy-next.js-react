3f996071c439873525bf89f33c5f7b9d2e045a13
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2023-08-19 17:30:08 -0300

    Projeto 01: Porta premiada - Componente Jogo

--- 00_projetos/portas/pages/index.tsx
+++ 00_projetos/portas/pages/index.tsx
@@ -5,18 +5,10 @@ import { atualizarPortas, criarPortas } from "@/functions/portas"
 
 export default function Home() {
 
-  const [portas, setPortas] = useState(criarPortas(4, 3))
-
-  function renderizaPortas() {
-    return portas.map(porta => {
-      return <Porta key={porta.numero} value={porta} 
-        onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))} />
-    })
-  }
 
   return (
     <div style={{display: "flex"}}>
-      {renderizaPortas()}  
+      <h1>Formulário de Início!!!</h1> 
     </div>
   )
 }
