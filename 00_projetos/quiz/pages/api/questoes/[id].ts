7d9f7873ad31e02f7120a717b7036441ee84be7c
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2023-08-24 20:53:32 -0300

    Projeto 02: Quiz - Banco de questões

--- 00_projetos/quiz/pages/api/questoes/[id].ts
+++ 00_projetos/quiz/pages/api/questoes/[id].ts
@@ -1,12 +1,9 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import questoes from './bancoDeQuestoes'
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.status(200).json({ 
-    id: req.query.id,
-    name: 'Wallace' 
-  })
+  res.status(200).json(questoes[0])
 }
