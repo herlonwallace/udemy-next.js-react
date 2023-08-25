35351116a055212097095cdbc31edd2d378fee60
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2023-08-25 18:47:59 -0300

    Projeto 02: Quiz - Respondendo a pergunta

--- 00_projetos/quiz/model/questao.ts
+++ 00_projetos/quiz/model/questao.ts
@@ -38,6 +38,16 @@ export default class QuestaoModel {
         return false
     }
 
+    responderCom(indice: number): QuestaoModel {
+        const acertou = this.#respostas[indice]?.certa
+        const respostas = this.#respostas.map((resposta, i) => {
+            const respostaSelecionada = indice === i
+            const deveRevelar = respostaSelecionada || resposta.certa
+            return deveRevelar ? resposta.revelar() : resposta 
+        }) 
+        return new QuestaoModel(this.id, this.enunciado, respostas, acertou)  
+    }
+
     embaralharRespostas() {
         let respostasEmbaralhadas = embaralhar(this.#respostas)
         return new QuestaoModel(this.#id, this.#enunciado, respostasEmbaralhadas, this.#acertou)
@@ -48,6 +58,7 @@ export default class QuestaoModel {
             id: this.#id,
             enunciado: this.#enunciado,
             respostas: this.#respostas.map(resp => resp.paraObjeto()),
+            respondida: this.respondida,
             acertou: this.#acertou
         }
     }
